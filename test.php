d6fbe0df018f192bde55af15583d131e1555738a
amrelngm6 <amr_elngm6@yahoo.com>
2023-05-13 14:57:32 +0200

    .

--- test.php
+++ test.php
@@ -4,13 +4,7 @@
 
 $ffmpeg = 'E:\Development\path\ffmpeg\bin\ffmpeg.exe';
 
-// // // Crop video
-foreach (glob('img/*.png') as $key => $value) {
-      $ex = explode('/', $value);
-      $nm = str_replace('.png', '.webp', end($ex));
-      // $nm = end($ex);
-      shell_exec($ffmpeg.' -i '.$value.' -c:v libwebp tmp/'.$nm);
-}
+shell_exec($ffmpeg.' -i '.$value.' -c:v libwebp tmp/'.$nm);
 
 return null;
 
