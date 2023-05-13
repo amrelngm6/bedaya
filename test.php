e2e0c9a238c8b7eda1a8e8d984e185b55424aa1d
amrelngm6 <amr_elngm6@yahoo.com>
2023-05-13 15:57:19 +0200

    .

--- test.php
+++ test.php
@@ -4,7 +4,14 @@
 
 $ffmpeg = 'E:\Development\path\ffmpeg\bin\ffmpeg.exe';
 
-shell_exec($ffmpeg.' -i '.$value.' -c:v libwebp tmp/'.$nm);
+foreach ([4,5,6,7,8] as $key => $value) 
+{
+	$i = 'uploads/img/online-0'.$value.'.webp';
+	$nm = 'online-0'.$value.'.webp';
+	shell_exec($ffmpeg.' -i '.$i.' -vf scale="80:-1" '.$nm);
+}
+
+shell_exec($ffmpeg.' -i uploads/img/operator.webp -vf scale="80:-1" operator.webp');
 
 return null;
 
