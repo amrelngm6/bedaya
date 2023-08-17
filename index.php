62c25558c7a7f3563afe5e8ab8d6f13ca40418c6
unknown <amr_elngm6@yahoo.com>
2023-08-17 08:44:42 +0300

    .

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start(); error_reporting(E_ALL);
+session_start(); error_reporting(0);
 date_default_timezone_set('Africa/Cairo');
 
 file_exists(__DIR__.'/app/config/database.php') ?  require_once __DIR__.'/app/config/database.php' : header('Location: ./installer/index.php');
@@ -13,22 +13,7 @@ spl_autoload_register(function ($name) {
     is_file($name2) ? include ($name2) : '';
 
 });
-// use phpseclib3\Net\SSH2;
 
-// if (isset($_GET['command']))
-// {
-
-// $ssh = new SSH2('41.36.145.38');
-// if (!$ssh->login('desktop-ddtq7ab\m ewais', '1')) {
-//     exit('Login Failed');
-// }
-
-// $ssh->exec('"C:/nircmd.exe" '.$_GET['command']);
-// // $ssh->exec('shutdown /r /t 0');
-// echo $_GET['command'];
-// $ssh->disconnect();
-
-// }
 
 /**
  * Set the database connection using 
