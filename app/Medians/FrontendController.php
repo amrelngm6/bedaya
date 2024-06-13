a8da64bb12aba01bc112f6502df7259f623352a3
unknown <amr_elngm6@yahoo.com>
2024-06-13 10:32:11 +0300

    Fix form issue

--- app/Medians/FrontendController.php
+++ app/Medians/FrontendController.php
@@ -36,10 +36,10 @@ class FrontendController extends CustomController
 	 */
 	public function form_submit($type)
 	{	
+		print_r($type);
+		print_r('type');
 		
 		$params = $this->app->request()->get('params');
-		print_r($type);
-		print_r($params);
 
 		try {
 				
